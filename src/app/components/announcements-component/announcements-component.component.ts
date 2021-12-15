1447e3ead6ff1b41562c94349522e1712bedd90c
unknown <sp-demo-admin@SP-DEMO.COM>
2021-12-15 17:46:46 +0000

    bootstrap icons registration changed

--- src/app/components/announcements-component/announcements-component.component.ts
+++ src/app/components/announcements-component/announcements-component.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AnnouncementsService } from 'src/app/services/announcements.service';
 import {Announcement} from "../../data/Announcement";
 
@@ -11,14 +12,26 @@ export class AnnouncementsComponentComponent implements OnInit {
 
   announcements: Announcement[] = [];
 
-  constructor(private service: AnnouncementsService) { }
+  constructor(private service: AnnouncementsService, @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
+      this.initIcons();
+      
       this.service.getAnnouncements()
       .subscribe(
         (result) => {
           this.announcements = result;
         });
+  }
+
+  initIcons(){
+    const head = this.document.getElementsByTagName('head')[0];
+    const style = this.document.createElement('link');
+    style.id = 'boottrap-icons';
+    style.rel = 'stylesheet';
+    style.href = `https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css`;
+
+    head.appendChild(style);
   }
 
 }
